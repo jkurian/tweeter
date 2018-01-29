39a94371387cdbb5c3e0e471dbe8f2bd26a8b01a
Jerry Kurian <jkurian770@gmail.com>
2018-01-29 14:34:17 -0500

    Updated variable names

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,11 +2,10 @@ $(document).ready(function () {
     console.log('ready');
 
     $('.new-tweet textarea')[0].addEventListener('keyup', function () {
-        let wordsLeft = $(this).val().split('');
-        console.log(wordsLeft);
-        $(this).parent().children('.counter').text(140 - wordsLeft.length);
-        
-        if(wordsLeft.length > 140) {
+        let lettersUsed = $(this).val().split('').length;
+        $(this).parent().children('.counter').text(140 - lettersUsed);
+
+        if(lettersUsed.length > 140) {
             $(this).parent().children('.counter').css('color', 'red');
         } else {
             $(this).parent().children('.counter').css('color', 'white');  
