0d9547c9831ba368e93499582ee1c92b760ab9cf
Jerry Kurian <jkurian770@gmail.com>
2018-01-29 14:45:32 -0500

    Moved hard coded word cap for a tweet to a const variable and also removed the array [0] since we specify the element
    Updated the change of color as if user was pasting blocks of text it would not change previously (go from 130 lettersUsed to 150 and our condition was lettersUsed === 140

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
+const maxLetters = 140;
+
 $(document).ready(function () {
     console.log('ready');
 
-    $('.new-tweet textarea')[0].addEventListener('keyup', function () {
-        let lettersUsed = $(this).val().split('').length;
+    $('.new-tweet textarea').on('keyup', function () {
+        let lettersUsed = $(this).val().length;
         $(this).parent().children('.counter').text(140 - lettersUsed);
 
-        if(lettersUsed === 141) {
+        if(lettersUsed > maxLetters) {
             $(this).parent().children('.counter').css('color', 'red');
-        } else if (lettersUsed === 140) {
+        } else if (lettersUsed <= maxLetters) {
             $(this).parent().children('.counter').css('color', 'white');  
         }
     });
