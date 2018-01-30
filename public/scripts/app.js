935a7f9a167ae5682cc068730ca7d421ee78e8d6
Jerry Kurian <jkurian770@gmail.com>
2018-01-30 13:47:41 -0500

    Added button to compose tweet and an event handler to toggle the section element (.slideToggle() on .new-tweet)

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,29 +14,42 @@ $(document).ready(function () {
     $(".new-tweet form").submit(function (event) {
         event.preventDefault();
         // console.log("Handler for submit called.");
+        //may have to change this! When sending scripts, it screws up
         let formDataSerialized = $(this).serialize();
+        //Should I use this here?
+        let tweetBody = $('.new-tweet form textarea[name="tweetText"]').val();
         // console.log(formDataSerialized);
-        if (formDataSerialized === null || formDataSerialized === "") {
+        if (tweetBody === null || tweetBody === "") {
             console.log("error");
             return;
         }
         //Need to update to flash animations!
-        if (formDataSerialized.length - 10 > 140) {
+        if (tweetBody.length > 140) {
             alert("Your tweet is too long!")
             return;
-        } else if (formDataSerialized.length - 10 === 0) {
+        } else if (tweetBody.length === 0) {
             alert("Your tweet is empty!");
             return;
         }
     
-        postNewTweet(name, handle, formDataSerialized, "");
+        postNewTweet(name, handle, escape(tweetBody), "");
         console.log("success"); // Only contains text field from tweet
 
     })
+
+    $("#nav-bar > .compose-tweet").on('click', function (event) {
+        // event.preventDefault();
+        $(".new-tweet").slideToggle(400, () => {
+            if($(".new-tweet").is(':visible')) {
+                $(".new-tweet form textarea").focus();
+            }
+        });
+    })
+
 });
 
-let postNewTweet = function (name, handle, formDataSerialized, date) {
-    let tweetObj = createNewTweetObject(name, handle, formDataSerialized, "");
+let postNewTweet = function (name, handle, tweetBody, date) {
+    let tweetObj = createNewTweetObject(name, handle, tweetBody, "");
     let $newTweet = createTweetElement(tweetObj);
 
     $(".container").append($newTweet)
@@ -44,7 +57,14 @@ let postNewTweet = function (name, handle, formDataSerialized, date) {
     //clear the form
     $(".new-tweet form textarea").val("");
 }
-let createNewTweetObject = function (name, handle, tweetBodySerialized, date) {
+
+let escape = function (str) {
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+}
+
+let createNewTweetObject = function (name, handle, tweetBody, date) {
     let tweetObj = {
         user: {
             name: name,
@@ -54,7 +74,7 @@ let createNewTweetObject = function (name, handle, tweetBodySerialized, date) {
             handle: handle
         },
         content: {
-            text: tweetBodySerialized.slice(10).replace(/\+/g, " ")
+            text: tweetBody
         },
         createdAt: ""
     };
@@ -82,7 +102,7 @@ let renderTweets = function (tweetArray) {
 
 let createHeader = function (tweet) {
     let $header = $("<header>").addClass("tweet-header");
-    
+
     let imgTag = "<img src=" + tweet.user.avatars.small + ">"
 
     $header.append(imgTag);
