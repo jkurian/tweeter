7ba4d2a2fecc51e8dd881d07d9084792874208f7
Jerry Kurian <jkurian770@gmail.com>
2018-01-30 11:38:12 -0500

    Refactored for modularity. Exported several functions outside of ready() function
    New tweets are now rendered on the page after sending form with postNewTweet(...) function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,31 +4,62 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready( function () {
+const name = "Jerry"
+const handle = "jkurian"
+const avatar = "https://ca.slack-edge.com/T2G8TE2E5-U8GHT8GBX-37cff5c2cda6-48"
+
+$(document).ready(function () {
     // renderTweets(data);
     loadTweets();
-    $(".new-tweet form").submit( function (event) {
+    $(".new-tweet form").submit(function (event) {
         event.preventDefault();
-        console.log("Handler for submit called.");
+        // console.log("Handler for submit called.");
         let formDataSerialized = $(this).serialize();
-        console.log(formDataSerialized);
-        if(formDataSerialized === null || formDataSerialized === "") {
+        // console.log(formDataSerialized);
+        if (formDataSerialized === null || formDataSerialized === "") {
             console.log("error");
             return;
         }
         //Need to update to flash animations!
-        if( formDataSerialized.length - 10 > 140) {
+        if (formDataSerialized.length - 10 > 140) {
             alert("Your tweet is too long!")
             return;
         } else if (formDataSerialized.length - 10 === 0) {
             alert("Your tweet is empty!");
             return;
         }
+    
+        postNewTweet(name, handle, formDataSerialized, "");
         console.log("success"); // Only contains text field from tweet
 
     })
-}); 
+});
 
+let postNewTweet = function (name, handle, formDataSerialized, date) {
+    let tweetObj = createNewTweetObject(name, handle, formDataSerialized, "");
+    let $newTweet = createTweetElement(tweetObj);
+
+    $(".container").append($newTweet)
+    addAnimations();
+    //clear the form
+    $(".new-tweet form textarea").val("");
+}
+let createNewTweetObject = function (name, handle, tweetBodySerialized, date) {
+    let tweetObj = {
+        user: {
+            name: name,
+            avatars: {
+                small: avatar
+            },
+            handle: handle
+        },
+        content: {
+            text: tweetBodySerialized.slice(10).replace(/\+/g, " ")
+        },
+        createdAt: ""
+    };
+    return tweetObj;
+}
 let loadTweets = function () {
     $.get("/tweets", function (data) {
         renderTweets(data);
@@ -37,13 +68,13 @@ let loadTweets = function () {
 }
 
 let addAnimations = function () {
-    $(".tweets").hover( function () {
+    $(".tweets").hover(function () {
         $("#footer-icons").toggleClass("animated bounce");
         $(this).toggleClass("animated bounce");
     })
 }
 let renderTweets = function (tweetArray) {
-    for(let tweet of tweetArray ) {
+    for (let tweet of tweetArray) {
         let $tweetElement = createTweetElement(tweet);
         $(".container").append($tweetElement);
     }
@@ -51,10 +82,13 @@ let renderTweets = function (tweetArray) {
 
 let createHeader = function (tweet) {
     let $header = $("<header>").addClass("tweet-header");
-    $header.append("<img>").attr("src", tweet.user.avatars.small)
+    
+    let imgTag = "<img src=" + tweet.user.avatars.small + ">"
+
+    $header.append(imgTag);
     $header.append("<h2>" + tweet.user.name);
     $header.append("<p>" + tweet.user.handle);
-    
+    // console.log("header = ", $header)
     return $header;
 }
 
@@ -66,7 +100,7 @@ let createFooter = function (tweet) {
     $footerIcons.append("<i class='material-icons'>favorite")
     $footerIcons.append("<i class='material-icons'>flag")
     $footer.append($footerIcons);
-    
+
     return $footer;
 }
 
@@ -81,7 +115,7 @@ let createTweetElement = function (tweet) {
 
     let $footer = createFooter(tweet);
     $innerArticle.append($footer);
-    
+
     $tweetArticle.append($innerArticle);
 
     return $tweetArticle;
