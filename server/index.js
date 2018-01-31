fb499f494bf776f482c77bdef8a276e4f910b7ff
Jerry Kurian <jkurian770@gmail.com>
2018-01-30 23:04:18 -0500

    Changed db URI to use myLab

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
 var MongoClient = require('mongodb').MongoClient;
-const MONGODB_URI = "mongodb://127.0.0.1:27017/tweeter";
+const MONGODB_URI = "mongodb://test:test@ds119688.mlab.com:19688/tweeter";
 console.log("CONNECTING TO MONGO");
 MongoClient.connect(MONGODB_URI, (err, db) => {
   if (err) {
